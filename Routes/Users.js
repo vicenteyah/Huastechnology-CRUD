1df2116381a99d56c093bfc74068c7d446256557
Vicente <vicente.yah.dzul@icloud.com>
2020-03-07 00:05:43 -0600

    code deleted due to redundancy

--- Routes/Users.js
+++ Routes/Users.js
@@ -89,49 +89,6 @@ users.post('/login', function(req, res) {
     })
 })
 
-users.post('/login', function(req, res) {
-
-    var appData = {}
-    var email = req.body.email
-    var password = req.body.password
-
-    database.connection.getConnection(function(err, connection) {
-        if (err) {
-            appData["error"] = 1
-            appData["data"] = "Internal Server Error"
-            res.status(500).json(appData)
-        } else {
-            connection.query('SELECT * FROM users WHERE email = ?', [email], function(err, rows, fields) {
-                if (err) {
-                    appData.error = 1
-                    appData["data"] = "Error Occured!"
-                    res.status(400).json(appData)
-                } else {
-                    if (rows.length > 0) {
-                        if (rows[0].password == password) {
-                            token = jwt.sign(rows[0], process.env.SECRET_KEY, {
-                                expiresIn: 5000
-                            });
-                            appData.error = 0
-                            appData["token"] = token
-                            res.status(200).json(appData)
-                        } else {
-                            appData.error = 1
-                            appData["data"] = "Email and Password does not match"
-                            res.status(204).json(appData)
-                        }
-                    } else {
-                        appData.error = 1
-                        appData["data"] = "Email does not exists!"
-                        res.status(204).json(appData)
-                    }
-                }
-            });
-            connection.release()
-        }
-    });
-});
-
 users.use(function(req, res, next) {
     var token = req.body.token || req.headers['token']
     var appData = {}
